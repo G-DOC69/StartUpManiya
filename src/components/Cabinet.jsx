dcf29ac44fcb4e06a82e65572e383255a1f8a59a
G-DOC69 <erlan.turar.5@gmail.com>
2024-02-15 00:23:53 +0600

    cabinet + ideas + full adaptive on both done

--- src/components/Cabinet.jsx
+++ src/components/Cabinet.jsx
@@ -1,8 +1,20 @@
 import '../styles/CabinetStyle.scss'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 
 const Cabinet = () => {
     const navigate = useNavigate();
+    const [showContainer1, setShowContainer1] = useState(true);
+    const [showContainer2, setShowContainer2] = useState(false);
+    const toggleContainer1 = () => {
+        setShowContainer1(true);
+        setShowContainer2(false);
+    };
+
+    const toggleContainer2 = () => {
+        setShowContainer1(false);
+        setShowContainer2(true);
+    };
     const user ={
         id: 1,
         tags:['UX/UI designer','Frontend разработчик','Проект менеджер','DEV OPS'],
@@ -42,6 +54,30 @@ const Cabinet = () => {
                 logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/2062095_application_chat_communication_logo_whatsapp_icon.svg/800px-2062095_application_chat_communication_logo_whatsapp_icon.svg.png',
                 link:'https://web.whatsapp.com/'
             }
+        ],
+        ideas:[
+            {
+                id:1,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },{
+                id:2,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },{
+                id:3,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },
         ]
     }
   return (
@@ -74,7 +110,9 @@ const Cabinet = () => {
                     {user.tags.map((tag, index) => (<li key={index} className={`tag`}>{tag}</li>))}
                 </ul>
             </div>
-            <div id="data_container">
+            <div className="data_container" style={{display: showContainer1?'flex':'none'}}>
+                <button className="data_button">Данные</button>
+                <button className="idea_button" onClick={toggleContainer2}>Идеи</button>
                 <a href='edit' id="edit_button">Изменить</a>
                 <div className="data_section">
                     <div className="data_block" style={{ backgroundColor: 'rgba(147,74,247,0.50)'}}>
@@ -119,6 +157,34 @@ const Cabinet = () => {
                             ))}
                         </div>
                     )}
+                </div>
+            </div>
+            <div className="data_container" style={{display: showContainer2?'flex':'none'}}>
+                <button className="data_button purple"onClick={toggleContainer1}>Данные</button>
+                <button className="idea_button yellow">Идеи</button>
+                <div id="idea_container">
+                    {user.ideas.map((idea,id)=>(
+                    <div key={id} className="idea_block">
+                        <div className="idea">
+                            <h3 className='idea_name'>{idea.name}</h3>
+                            <p className='idea_description'>{idea.description}</p>
+                            <ul className='idea_tags'>
+                                {idea.hashtags.map((hashtag,index)=>(<a value={hashtag} key={index} className='idea_tag'>{hashtag}</a>))}
+                            </ul>
+                        </div>
+                        <div className="idea_interactables">
+                            <div className="idea_interactable" style={{color:'#934AF7'}}>
+                                <img width={40} height={34} src="../src/assets/CabinetAssets/follow.svg" alt=""/>
+                                {idea.developers}
+                            </div>
+                            <div className="idea_interactable"
+                            style={{color:'#DD403F'}}>
+                                <img width={40} height={34} src="../src/assets/CabinetAssets/like.svg" alt="" />
+                                {idea.likes}
+                            </div>
+                        </div>
+                    </div>
+                    ))}
                 </div>
             </div>
         </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -24,27 +24,49 @@ const Profile = () => {
             link: 'https://google.com'
           }
         ],
-        socials: [
-          {
-            id: 1,
-            name: 'Instagram',
-            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/198px-Instagram_logo_2016.svg.png?20210403190622',
-            link: 'https://www.instagram.com/svekolnikovsergej7/'
-          },
-          {
-            id: 2,
-            name: 'VK',
-            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/2048px-VK_Compact_Logo_%282021-present%29.svg.png',
-            link: 'https://vk.com/'
-          },
-          {
-            id: 3,
-            name: '+1234567890',
-            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/2062095_application_chat_communication_logo_whatsapp_icon.svg/800px-2062095_application_chat_communication_logo_whatsapp_icon.svg.png',
-            link: 'https://web.whatsapp.com/'
-          }
+        socials:[
+            {
+                id:1,
+                name:'Instagram',
+                logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/198px-Instagram_logo_2016.svg.png?20210403190622',
+                link:'https://www.instagram.com/svekolnikovsergej7/'
+            },{
+                id:2,
+                name:'VK',
+                logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/2048px-VK_Compact_Logo_%282021-present%29.svg.png',
+                link:'https://vk.com/'
+            },{
+                id:3,
+                name:'+996706182355',
+                logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/2062095_application_chat_communication_logo_whatsapp_icon.svg/800px-2062095_application_chat_communication_logo_whatsapp_icon.svg.png',
+                link:'https://web.whatsapp.com/'
+            }
+        ],
+        ideas:[
+            {
+                id:1,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },{
+                id:2,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },{
+                id:3,
+                name:'Lorem ipsum dolor sit amet, consectetur.',
+                description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae semper nisl, non ultricies turpis. Etiam mollis orci sit amet quam pharetra, non pretium.',
+                hashtags:['#frontend','#react','#college'],
+                developers:3,
+                likes:2
+            },
         ]
-      };      
+    }      
   return (
     <div id="cabinet">
         <div id="background_container">
@@ -75,7 +97,9 @@ const Profile = () => {
                     {user.tags.map((tag, index) => (<li key={index} className={`tag`}>{tag}</li>))}
                 </ul>
             </div>
-            <div id="data_container">
+            <div className="data_container" style={{display: showContainer1?'flex':'none'}}>
+                <button className="data_button">Данные</button>
+                <button className="idea_button" onClick={toggleContainer2}>Идеи</button>
                 <div className="data_section">
                     <div className="data_block" style={{ backgroundColor: 'rgba(147,74,247,0.50)'}}>
                         <h1 className='data_header'>Данные</h1>
@@ -97,7 +121,7 @@ const Profile = () => {
                     )}
                 </div>
                 <div className="data_section">
-                {user.works.length > 0 && (
+                    {user.works.length > 0 && (
                         <div className="data_block" style={{ backgroundColor: 'rgba(251,184,0,0.50)'}}>
                             <h1 className='data_header'>Работы</h1>
                             {user.works.map((work,id)=>(
@@ -119,6 +143,34 @@ const Profile = () => {
                             ))}
                         </div>
                     )}
+                </div>
+            </div>
+            <div className="data_container" style={{display: showContainer2?'flex':'none'}}>
+                <button className="data_button purple"onClick={toggleContainer1}>Данные</button>
+                <button className="idea_button yellow">Идеи</button>
+                <div id="idea_container">
+                    {user.ideas.map((idea,id)=>(
+                    <div key={id} className="idea_block">
+                        <div className="idea">
+                            <h3 className='idea_name'>{idea.name}</h3>
+                            <p className='idea_description'>{idea.description}</p>
+                            <ul className='idea_tags'>
+                                {idea.hashtags.map((hashtag,index)=>(<a value={hashtag} key={index} className='idea_tag'>{hashtag}</a>))}
+                            </ul>
+                        </div>
+                        <div className="idea_interactables">
+                            <div className="idea_interactable" style={{color:'#934AF7'}}>
+                                <img width={40} height={34} src="../src/assets/CabinetAssets/follow.svg" alt=""/>
+                                {idea.developers}
+                            </div>
+                            <div className="idea_interactable"
+                            style={{color:'#DD403F'}}>
+                                <img width={40} height={34} src="../src/assets/CabinetAssets/like.svg" alt="" />
+                                {idea.likes}
+                            </div>
+                        </div>
+                    </div>
+                    ))}
                 </div>
             </div>
         </div>
